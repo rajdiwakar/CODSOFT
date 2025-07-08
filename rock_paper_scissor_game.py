f9fe51560982d00f5cb7231e444fc6746ee08445
RAJ DIWAKAR <rajdiwakar9161@gmail.com>
2025-07-08 12:46:13 +0530

    rock_paper_scissor_game.py

--- rock_paper_scissor_game.py
+++ rock_paper_scissor_game.py
@@ -1,34 +1,37 @@
 # Rock, Paper and Scissor Game
 
 import random
+
 item_list = ["Rock", "Paper", "Scissor"]
 
-user_choice = input(" Enter youe move = Rock, Paper, Scissor : ")
+user_choice = input("Enter your move = Rock, Paper, Scissor : ")
 
 computer_choice = random.choice(item_list)
 
 print("User Choice = ",user_choice)
-print("Computer Choice = ",computer_choice)
-
+print("Computer choice = ",computer_choice)
 
 if user_choice == computer_choice:
-    print("Both chooses same: = Match Tie ")
-    
+    print("Both Chooses Same : = Match Tie ")
+
 elif user_choice == "Rock":
     if computer_choice == "Paper":
-        print("Paper covers Rock = Computer Win")
+        print("Paper cover Rock = Computer Win")
+
     else:
-        print("Rock smashes Scissor = You Win")
-                 
+        print("Rock smashes Scissor = You Win ")
+
 elif user_choice == "Paper":
-    if computer_choice == "Scissor":
-        print("Scissor cuts Paper = Computer Win")
+    if computer_choice== "Scissor":
+        print("Scissor cuts Paper = Computer Win ")   
+
     else:
-        print("Paper covers Rock = You Win")
+        print("Paper covers Rock = You Win ")
 
 elif user_choice == "Scissor":
     if computer_choice == "Paper":
-        print("Scissor cuts Paper = You Win")
+        print("Scissor cuts Paper = You win ")
+
     else:
-        print("Rock smashes Scissor = Computer Win")                                 
+        print("Rock smashes Scissor = Computer Win")                                     
                  
