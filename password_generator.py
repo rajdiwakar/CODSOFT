038578a9cd07c8b8404374fdbe4b7599a4ce748b
RAJ DIWAKAR <rajdiwakar9161@gmail.com>
2025-07-08 12:44:18 +0530

    password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -2,28 +2,29 @@ import string
 import random
 
 try:
-    pass_generator =  int(input("Enter the password length : "))
-    
+    pass_generator = int(input("Enter the password length : "))
+
     if pass_generator < 4 :
-        print(" Password lenght should be at least 4 characters.")
+        print("Password length should be at least 4 characters.")
+
 
     else:
-        
+
         s1 = string.ascii_lowercase
         s2 = string.ascii_uppercase
         s3 = string.digits
         s4 = string.punctuation
 
-            
         s = []
         s.extend(list(s1))
         s.extend(list(s2))
         s.extend(list(s3))
         s.extend(list(s4))
 
-        print("Your password is :")
-        print("".join(random.sample(s,pass_generator)))
-        
+
+        print("Your password is : ")
+        print("".join(random.sample(s,pass_generator)))    
+
 except ValueError:
-     print("Invalid input. Please enter a number.")        
+    print("Invalid input. Please enter a number.")  
     
